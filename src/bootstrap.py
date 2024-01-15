3a49da1b573b4d86d3186b696ee2416b7992a9c3
Alexander D Steele <Alexander.Steele.2014@outlook.com>
2024-01-14 23:32:49 -0600

    Refactor main application logic to new bootstrap module

---
+++ src/bootstrap.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from pathlib import Path
+
+from src.datatypes.address import Address
+from src.datatypes.truck import Truck
+from src.services.package_loader import package_loader
+from src.services.pkg_pool import update_pkg_pool
+from src.services.truck_loader import load_trucks
+from src.services.update_links import update_links
+from src.utils.csv_parser import read_csv
+
+
+def bootstrap():
+    base_path = Path.cwd()
+
+    # Assign data file locations
+    packages_file = base_path / "data/package_data.csv"
+    address_file = base_path / "data/address_file.csv"
+    distance_file = base_path / "data/distance_data.csv"
+
+    # Instantiate databases
+    addresses = []
+    distance_matrix = []
+
+    # Parse addresses csv file and store in a list
+    data = read_csv(address_file)
+    for row in data:
+        index = int(row[0])
+        name = row[1]
+        address = row[2]
+        addresses.append(Address(index, name, address))
+
+    # Parse distance matrix csv and store in a 2D list
+    data = read_csv(distance_file)
+    for row in data:
+        distance_matrix.append(row)
+    distance_matrix = [[float(cell) if cell else None for cell in row] for row in distance_matrix]
+
+    # Instantiate two trucks from the assignment
+    truck_1 = Truck(1)
+    truck_2 = Truck(2)
+    trucks = [truck_1, truck_2]
+    minimum_speed = min([truck.speed_mph for truck in trucks])
+    maximum_capacity = min([truck.max_inv for truck in trucks])
+
+    # Day Start
+    day_start = datetime.strptime("8:00 am", '%I:%M %p')
+    hub = addresses[0]
+
+    # Load packages at the start of the day
+    packages = package_loader(addresses, packages_file, trucks, "8:00 am")
+    packages_set = set()
+    for key, package in packages:
+        packages_set.add(package)
+    update_links(packages_set)
+
+    # Load package pool
+    package_history = set()
+    depot = update_pkg_pool(packages_set, day_start, package_history)
+
+    # Load truck loads
+    load_trucks(depot, trucks)
+    for truck in trucks:
+        for package in truck.inv:
+            package_history.add(package.package_id)
--- src/main.py
+++ src/main.py
@@ -11,20 +11,10 @@
 # '----------------------------------------------------------------------------------------------------------'
 # By Alexander D. Steele ------------------------------- Student ID: 011226486 -------------- DATE: 1/14/2024
 import threading
-from datetime import datetime
-from pathlib import Path
 
-from src.datatypes.address import Address
-from src.datatypes.truck import Truck
-from src.services.cwsa import calculate_savings, combine_routes
-from src.services.package_loader import package_loader
-from src.services.pkg_pool import update_pkg_pool
-from src.services.truck_loader import load_trucks
-from src.services.update_links import update_links
+from src.bootstrap import bootstrap
 from src.ui.load_animation import loading_animation
 from src.ui.ui_methods import display_title, display_credit
-from src.utils.csv_parser import read_csv
-from src.utils.distance_functions import get_distance
 
 if __name__ == '__main__':
 
@@ -37,84 +27,10 @@ if __name__ == '__main__':
     load = threading.Thread(target=loading_animation, args=(stop,))
     load.start()
 
-    base_path = Path.cwd()
-
-    # Assign data file locations
-    packages_file = base_path / "data/package_data.csv"
-    address_file = base_path / "data/address_file.csv"
-    distance_file = base_path / "data/distance_data.csv"
-
-    # Instantiate databases
-    addresses = []
-    distance_matrix = []
-
-    # Parse addresses csv file and store in a list
-    data = read_csv(address_file)
-    for row in data:
-        index = int(row[0])
-        name = row[1]
-        address = row[2]
-        addresses.append(Address(index, name, address))
-
-    # Parse distance matrix csv and store in a 2D list
-    data = read_csv(distance_file)
-    for row in data:
-        distance_matrix.append(row)
-    distance_matrix = [[float(cell) if cell else None for cell in row] for row in distance_matrix]
-
-    # Instantiate two trucks from the assignment
-    truck_1 = Truck(1)
-    truck_2 = Truck(2)
-    trucks = [truck_1, truck_2]
-    minimum_speed = min([truck.speed_mph for truck in trucks])
-    maximum_capacity = min([truck.max_inv for truck in trucks])
-
-    # Day Start
-    day_start = datetime.strptime("8:00 am", '%I:%M %p')
-    hub = addresses[0]
-
-    # Load packages at the start of the day
-    packages = package_loader(addresses, packages_file, trucks, "8:00 am")
-    packages_set = set()
-    for key, package in packages:
-        packages_set.add(package)
-    update_links(packages_set)
-
-    # Load package pool
-    package_history = set()
-    depot = update_pkg_pool(packages_set, day_start, package_history)
-
-    # Load truck loads
-    load_trucks(depot, trucks)
-    for truck in trucks:
-        for package in truck.inv:
-            package_history.add(package.package_id)
+    # Run bootstrap
+    bootstrap()
 
     # Stop the loading animation thread
     stop.set()
     load.join()
     print("\n")
-
-    print(f"Time: {str(datetime)}")
-
-    for truck in trucks:
-        print("Truck " + str(truck.truck_id) + ": ")
-        for pkg in truck.inv:
-            print(pkg)
-
-    print("At Depot: ")
-    for pkg in depot:
-        print(pkg)
-
-    truck_1_savings = calculate_savings(truck_1.inv, distance_matrix, hub)
-    for pkgs, saving in truck_1_savings:
-        print(f"Packages: {pkgs[0].package_id} & {pkgs[1].package_id}\tSavings: {saving}")
-
-    truck_1_route = combine_routes(truck_1_savings, truck_1.inv)
-    print(f"{hub}\t--> {get_distance(distance_matrix, hub, truck_1_route[0][0])}")
-    for route in truck_1_route:
-        for idx, ad in enumerate(route):
-            if ad != route[-1]:
-                print(f"{ad}\t--> {get_distance(distance_matrix, ad, route[idx+1])}")
-            else:
-                print(f"{ad}\t--> {get_distance(distance_matrix, ad, hub)}")
